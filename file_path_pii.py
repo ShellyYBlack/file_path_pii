b93d071fd3ce2ec379a75fc2d6aaf95d8d3c1ccf
ShellyYBlack <syblack@ncsu.edu>
2023-04-08 16:48:05 -0400

    Update script

--- file_path_pii.py
+++ file_path_pii.py
@@ -1,25 +1,30 @@
+import re
 import os
-
-# https://www.techbeamers.com/program-python-list-contains-elements/
-# https://blog.finxter.com/how-to-check-if-items-in-a-python-list-are-also-in-another/
 
 piilist = []
 with open("find.txt", "r") as piidoc:
-    for pii in piidoc:
-        piilist = piidoc.read().splitlines()
+    piilist = (piidoc.read().splitlines())
 #print(piilist)
 
 filepathlist = []
-for dirpath, dirnames, filenames in os.walk("/syblack/Documents/pii_file_path/testfiles"):
+for dirpath, dirnames, filenames in os.walk("/syblack/Documents/file_path_pii/testfiles"):
     for file in filenames:
         filepathlist.append(os.path.join(dirpath, file))
 #print(filepathlist)
 
-results = []
-for i in piilist:
-    for j in filepathlist:
-        if i in j:
-            results.append(j)
-        else:
-            continue
-print(results)
+for stringpattern in piilist:
+    regex = re.compile(stringpattern)
+    for f in filepathlist:
+        # how to not print empty matches??
+        result = re.findall(regex, f)
+        print(f,",".join(result))
+
+
+# results = []
+# for i in piilist:
+#     for j in filepathlist:
+#         if i in j:
+#             results.append(j)
+#         else:
+#             continue
+# print(results)
