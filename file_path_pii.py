325ca0add0c0677c88ef0937a5d8ae6f1e471574
ShellyYBlack <syblack@ncsu.edu>
2023-07-08 13:35:49 -0400

    Use re.search in script

--- file_path_pii.py
+++ file_path_pii.py
@@ -3,26 +3,21 @@ import re, os, csv
 piilist = []
 with open("rlist.txt", "r") as piidoc:
     piilist = (piidoc.read().splitlines())
-#print(piilist)
 
 filepathlist = []
 for dirpath, dirnames, filenames in os.walk("/syblack/Documents/file_path_pii/testfiles"):
     for file in filenames:
         filepathlist.append(os.path.join(dirpath, file))
-#print(filepathlist)
 
 resultlist = []
 for stringpattern in piilist:
     regex = re.compile(stringpattern)
     for f in filepathlist:
-        result = re.findall(regex, f)
-        if not result == []:
-            # print(f,",".join(result))
-            resulttuples = f,",".join(result)
-            # print(resulttuples)
-            resultlist.insert(1,resulttuples)
-            print(resultlist)
+        result = re.search(regex, f)
+        if not result == None:
+            for x in zip([result.string], [result.group()]):
+                resultlist.append(x)
             with open('file_path_pii.csv', mode='w') as result_file:
                 writer = csv.writer(result_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 for row in resultlist:
-                    writer.writerow(row)             
+                    writer.writerow(row)
